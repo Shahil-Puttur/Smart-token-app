e28d307c83d81237fa37a49f42703fc18e5e7e43
Shahil Puttur <rajubhai59647@gmail.com>
2025-07-22 19:22:06 +0530

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// ✅ Filled with your Firebase project: smart-hospital-51bf4
+const firebaseConfig = {
+    apiKey: "AIzaSyD9ZRUuTQ-x9HTgbZZZk8xXrKL6MoEtHJ4",
+    authDomain: "smart-hospital-51bf4.firebaseapp.com",
+    databaseURL: "https://smart-hospital-51bf4-default-rtdb.firebaseio.com",
+    projectId: "smart-hospital-51bf4",
+    storageBucket: "smart-hospital-51bf4.appspot.com",
+    messagingSenderId: "1098223271895",
+    appId: "1:1098223271895:web:57b65c6c2eebdc09a76b8b"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.database();
